00ce82281f05991ba28f69399faa3e1e31f92ff6
tandai <tandai@LTD-MacBook-Pro.local>
2022-08-06 18:33:46 +0700

    add navigation with navigation component and react router link

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import Directory from "./components/directory/directory.component";
+import { Route, Routes } from "react-router-dom";
+import Home from './components/routes/home/Home.component';
+import Navigation from './components/routes/navigation/navigation.component';
+
+export const Shop = () => {
+  return <h1>This is a SHOP!</h1>
+}
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation></Navigation>}>
 
-  return <Directory categories={categories} />;
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  )
 };
 
 export default App;
